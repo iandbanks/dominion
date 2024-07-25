59d9cd94d711e2ddf845c087278cb9a1526ad39e
Ian Banks <iandavidbanks@gmail.com>
2024-07-24 22:55:32 -0500

    chore: Update eslint and related dependencies, fix card skeleton styles

--- src/components/skeletons/cards/card/CardSkeleton.jsx
+++ src/components/skeletons/cards/card/CardSkeleton.jsx
@@ -2,7 +2,7 @@
 
 const CardSkeleton = () => {
   return (
-    <div className="card-skeleton p-5 bg-gray-500 border border-slate-400 rounded-md">
+    <div className="card-skeleton p-5 bg-gray-600 border border-slate-400 rounded-md">
       {/* This is the header skeleton */}
       <div className="animate-pulse">
         <div className="flex flex-col items-center justify-between">
@@ -12,7 +12,8 @@ const CardSkeleton = () => {
           {/* This is the avatar skeleton */}
           <div className="w-60 h-48"></div>
           <div className="w-60 h-32 bg-gray-400 rounded-t"></div>
-          <div className="w-64 py-3 bg-gray-400 flex items-center rounded-sm justify-center">
+          <div className="w-64 py-3 bg-gray-400 flex items-center rounded-sm justify-center relative">
+            <div className="w-6 h-6 bg-gray-500 py-2 rounded-full absolute left-1"></div>
             <div className="w-20 bg-gray-500 py-1 center rounded"></div>
           </div>
         </div>
